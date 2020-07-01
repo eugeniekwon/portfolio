15c57cbc3d3e298ec4379411b8709fa29924bfb4
eugeniekwon <eugenie.kwon@gmail.com>
2020-07-02 02:55:18 +0900

    edits to homepage

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,24 +1,22 @@
-
 var $burger = $("#burger-icon");
 var $nav = $('nav[data-nav]');
 var $navA = $('nav[data-nav] a');
 var isVisible = false;
- 
-function mouseClick(){
- 
- if($(window).width() <= 992){
- console.log("click");
- if(isVisible === false){
- $nav.show();
- isVisible = true;
- }
- else{
- $nav.hide();
- isVisible = false;
- }
- } 
+
+function mouseClick() {
+    console.log("click");
+    if ($(window).width() <= 992) {
+        console.log("click");
+        if (isVisible === false) {
+            $nav.show();
+            isVisible = true;
+        } else {
+            $nav.hide();
+            isVisible = false;
+        }
+    }
 }
- 
-$burger.on( "click", mouseClick);
+
+$burger.on("click", mouseClick);
 // close the mobile menu when menu link is clicked
-$navA.on( "click", mouseClick);
+$navA.on("click", mouseClick);
