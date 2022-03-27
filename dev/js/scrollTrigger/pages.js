c77aeb962a7bcc6902168f2442878a5591687aae
eugeniekwon <eugenie.kwon@gmail.com>
2022-03-27 02:22:13 -0400

    edits - book covers

--- dev/js/scrollTrigger/pages.js
+++ dev/js/scrollTrigger/pages.js
@@ -664,8 +664,8 @@ if(bookCoversPage){
                  .from("#book-covers #white-teeth div ul li:nth-child(3)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#book-covers #white-teeth div ul li:nth-child(4)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#book-covers #white-teeth div ul li:nth-child(5)", {duration:pageTime, alpha:0},"-=0.01")
-                 .from("#book-covers #white-teeth div ul li:nth-child(6)", {duration:pageTime, alpha:0},"-=0.01")
-                 .from("#book-covers #white-teeth div ul li:nth-child(7)", {duration:pageTime, alpha:0},"-=0.01")
+                //  .from("#book-covers #white-teeth div ul li:nth-child(6)", {duration:pageTime, alpha:0},"-=0.01")
+                //  .from("#book-covers #white-teeth div ul li:nth-child(7)", {duration:pageTime, alpha:0},"-=0.01")
 
                  .from("#book-covers #book-total div ul li:nth-child(1)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#book-covers #book-total div ul li:nth-child(2)", {duration:pageTime, alpha:0},"-=0.01")
