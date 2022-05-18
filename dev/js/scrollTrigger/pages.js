fadb801eef08581828ef74e92d766d6e74403ac4
eugeniekwon <eugenie.kwon@gmail.com>
2022-05-18 16:23:18 -0400

    revised frank auerbach layout

--- dev/js/scrollTrigger/pages.js
+++ dev/js/scrollTrigger/pages.js
@@ -948,14 +948,14 @@ if(frankAuerbachPage){
 
                  .from("#frank-auerbach #frank-auerbach-start div ul li:nth-child(1)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#frank-auerbach #frank-auerbach-start div ul li:nth-child(2)", {duration:pageTime, alpha:0},"-=0.01")
-                //  .from("#frank-auerbach #frank-auerbach-start div ul li:nth-child(3)", {duration:pageTime, alpha:0},"-=0.01")
+                 .from("#frank-auerbach #frank-auerbach-start div ul li:nth-child(3)", {duration:pageTime, alpha:0},"-=0.01")
 
                  .from("#frank-auerbach #frank div ul li:nth-child(1)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#frank-auerbach #frank div ul li:nth-child(2)", {duration:pageTime, alpha:0},"-=0.01")
 
                  .from("#frank-auerbach #estella div ul li:nth-child(1)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#frank-auerbach #estella div ul li:nth-child(2)", {duration:pageTime, alpha:0},"-=0.01")
-                 .from("#frank-auerbach #estella div ul li:nth-child(3)", {duration:pageTime, alpha:0},"-=0.01")
+                //  .from("#frank-auerbach #estella div ul li:nth-child(3)", {duration:pageTime, alpha:0},"-=0.01")
                 //  .from("#frank-auerbach #estella div ul li:nth-child(4)", {duration:pageTime, alpha:0},"-=0.01")
 
                  .from("#frank-auerbach #juliet div ul li:nth-child(1)", {duration:pageTime, alpha:0},"-=0.01")
