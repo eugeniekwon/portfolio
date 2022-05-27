28243da8ae0f2eb66a3929d0c17379526f70a1e1
eugeniekwon <eugenie.kwon@gmail.com>
2022-05-27 15:47:46 -0400

    book cover edits

--- dev/js/scrollTrigger/pages.js
+++ dev/js/scrollTrigger/pages.js
@@ -812,7 +812,7 @@ if(bookCoversPage){
                  .from("#book-covers #orlando div ul li:nth-child(4)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#book-covers #orlando div ul li:nth-child(5)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#book-covers #orlando div ul li:nth-child(6)", {duration:pageTime, alpha:0},"-=0.01")
-                 .from("#book-covers #orlando div ul li:nth-child(7)", {duration:pageTime, alpha:0},"-=0.01")
+                //  .from("#book-covers #orlando div ul li:nth-child(7)", {duration:pageTime, alpha:0},"-=0.01")
                 //  .from("#book-covers #orlando div ul li:nth-child(8)", {duration:pageTime, alpha:0},"-=0.01")
 
                 .from("#book-covers #book-covers-main-2 div h2", {duration:pageIntroTime, alpha:0}, "+=0.3")
