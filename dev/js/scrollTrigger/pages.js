4540b110f529cca077ecbbd9975081857d80fe90
eugeniekwon <eugenie.kwon@gmail.com>
2022-05-31 22:59:12 -0400

    sleepground strategy gif layout changed

--- dev/js/scrollTrigger/pages.js
+++ dev/js/scrollTrigger/pages.js
@@ -450,9 +450,9 @@ if(thesleepgroundPage){
                  .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(7)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(8)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(9)", {duration:pageTime, alpha:0},"-=0.01")
-                 .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(10)", {duration:pageTime, alpha:0},"-=0.01")
-                 .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(11)", {duration:pageTime, alpha:0},"-=0.01")
-                 .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(12)", {duration:pageTime, alpha:0},"-=0.01")
+                //  .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(10)", {duration:pageTime, alpha:0},"-=0.01")
+                //  .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(11)", {duration:pageTime, alpha:0},"-=0.01")
+                //  .from("#the-sleepground #the-sleepground-strategy div ul li:nth-child(12)", {duration:pageTime, alpha:0},"-=0.01")
 
                  .from("#the-sleepground #the-sleepground-prearrival div ul li:nth-child(1)", {duration:pageTime, alpha:0},"-=0.01")
                  .from("#the-sleepground #the-sleepground-prearrival div ul li:nth-child(2)", {duration:pageTime, alpha:0},"-=0.01")
